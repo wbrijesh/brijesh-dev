1911c834f36a3fb0e62d92a76ea6ac018b983c81
Brijesh Wawdhane <brijesh@wawdhane.com>
2022-01-13 17:01:06 +0530

    install tailwind
    Signed-off-by: Brijesh Wawdhane <brijesh@wawdhane.com>

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -5,7 +5,4 @@ module.exports = {
       rootDir: ["apps/*/", "packages/*/"],
     },
   },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
 };
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,7 @@
-import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return (
+    <button className="bg-blue-300 px-2 py-1.5 rounded focus:ring-2 focus:ring-offset-2">
+      Boop
+    </button>
+  );
 };
